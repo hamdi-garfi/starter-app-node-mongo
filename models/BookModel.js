26a343086a4a439521120b85338593d4e5845aea
hamdi <garfi.hamdi@gmail.com>
2020-11-03 04:16:36 +0100

    add required to book schema

--- models/BookModel.js
+++ models/BookModel.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 
 var BookSchema = new Schema({
 	title: {type: String, required: true},
+	
 	description: {type: String, required: true},
 	isbn: {type: String, required: true},
 	user: { type: Schema.ObjectId, ref: "User", required: true },
